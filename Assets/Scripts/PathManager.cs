909fcc1a4f6b63eaac517edb29ffb4a614523767
s231880 <casu.tiziano@gmail.com>
2020-12-26 17:46:22 +0000

    Platform Movements

--- Assets/Scripts/PathManager.cs
+++ Assets/Scripts/PathManager.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
 using UnityEngine;
 
 namespace PenguinRun
@@ -8,7 +10,8 @@ namespace PenguinRun
     {
         private const string PATH = "Path";
         private const int NUM_OF_PATH = 3;
-        private const int NUM_OF_ELEMENTS = 5;
+        private const int NUM_OF_ELEMENTS = 10;
+        private PathElement m_LastElement;
         private Vector3 m_ElementsStartingPoint = Vector3.zero;
         private List<GameObject> m_PathList = new List<GameObject>();
         private List<PathElement> m_ActiveElements = new List<PathElement>();
@@ -18,7 +21,7 @@ namespace PenguinRun
 
         private void Update()
         {
-
+            UpdatePath();
         }
 
         public void Initialise(float bottomRightScreenCornerX)
@@ -68,6 +71,7 @@ namespace PenguinRun
 
             m_ElementsStartingPoint = element.transform.position;
             m_ElementsStartingPoint.x = bottomRightScreenCornerX + (m_ElementLenght / 2);
+            Debug.LogError($"{m_ElementsStartingPoint.x} + {bottomRightScreenCornerX} + {m_ElementLenght / 2}");
         }
 
         IEnumerator InitialiseScene()
@@ -80,20 +84,42 @@ namespace PenguinRun
             var element = m_Pool.GetObject().GetComponent<PathElement>();
             element.Activate(m_ElementsStartingPoint, pathSpeed);
             m_ActiveElements.Add(element);
+            m_LastElement = element;
 
-
-            Vector3 secondStartingPoint = m_ElementsStartingPoint;
-            secondStartingPoint.x -= (m_ElementLenght / 2);
-            var secondElement = m_Pool.GetObject().GetComponent<PathElement>();
-            secondElement.Activate(secondStartingPoint, pathSpeed);
-            m_ActiveElements.Add(secondElement);
-
-            Vector3 thirdStartingPoint = secondStartingPoint;
+            Vector3 thirdStartingPoint = m_ElementsStartingPoint;
             thirdStartingPoint.x -= m_ElementLenght;
             var thirdElement = m_Pool.GetObject().GetComponent<PathElement>();
             thirdElement.Activate(thirdStartingPoint, pathSpeed);
             m_ActiveElements.Add(thirdElement);
         }
 
+        private void UpdatePath()
+        {
+            foreach (var path in m_ActiveElements)
+            {
+                if (path.transform.position.x < -m_ElementsStartingPoint.x)
+                {
+                    ReturnElement(path);
+                    GetNewElement();
+                    break;
+                }
+            }
+        }
+
+        private void ReturnElement(PathElement path)
+        {
+            m_Pool.ReturnObjectToThePool(path.gameObject);
+            m_ActiveElements.Remove(path);
+        }
+
+        private void GetNewElement()
+        {
+            var element = m_Pool.GetObject().GetComponent<PathElement>();
+            Vector3 pos = element.transform.position;
+            pos.x = m_LastElement.transform.position.x + m_ElementLenght;
+            element.Activate(pos, pathSpeed);
+            m_ActiveElements.Add(element);
+            m_LastElement = element;
+        }
     }
 }
